985086e87e2f6ce2f3d889f99f78be1160c28c30
Anto <antodeb36@gmail.com>
2025-09-19 19:49:24 +0000

    Add This Popunder Add <script type='text/javascript' src='//pl27681453.r

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,13 +18,15 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;5
+00;600;700&display=swap"
           rel="stylesheet"
         />
       </head>
       <body className="font-body antialiased">
         {children}
         <Toaster />
+        <script type='text/javascript' src='//pl27681453.revenuecpmgate.com/08/8a/35/088a35a8a3c9deffd7984b220d0ad4d4.js'></script>
       </body>
     </html>
   );
